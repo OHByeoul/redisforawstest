cc077dd5f36351dda3ac7beb3502e1b7b1749b9d
OHByeoul <suriessay@naver.com>
2021-09-27 19:01:58 +0900

    cd test

--- src/main/java/com/example/redisforawstest/controller/RedisController.java
+++ src/main/java/com/example/redisforawstest/controller/RedisController.java
@@ -3,10 +3,17 @@ package com.example.redisforawstest.controller;
 import com.example.redisforawstest.service.RedisService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class RedisController {
     private final RedisService redisService;
+
+    @GetMapping("/")
+    public String test() {
+        redisService.redisString();
+        return "tttttest";
+    }
 }
--- src/main/java/com/example/redisforawstest/service/RedisService.java
+++ src/main/java/com/example/redisforawstest/service/RedisService.java
@@ -1,15 +1,21 @@
 package com.example.redisforawstest.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RedisService {
     private final RedisTemplate<String, Object> redisTemplate;
 
-//    public void redisString() {
-//        ValueOperations<String, Object>
-//    }
+    public void redisString() {
+        ValueOperations<String, Object> operations = redisTemplate.opsForValue();
+        operations.set("test","test");
+        String redis = operations.get("test").toString();
+        log.info(redis);
+    }
 }
